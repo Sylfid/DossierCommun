068a4ef45a8e88e6dca137ef96be45e3e0ec6069
Sylfid <remy.lavainne@laposte.net>
2019-03-05 21:12:12 +0100

    TP2Exo2

--- Grapique3D/viewer.py
+++ Grapique3D/viewer.py
@@ -3,17 +3,20 @@
 Python OpenGL practical application.
 """
 # Python built-in modules
-import os                           # os function, i.e. checking file status
+import os                   # os function, i.e. checking file status
 
 # External, non built-in modules
-import OpenGL.GL as GL              # standard Python OpenGL wrapper
-import glfw                         # lean window system wrapper for OpenGL
-import numpy as np                  # all matrix manipulations & OpenGL args
+import OpenGL.GL as GL      # standard Python OpenGL wrapper
+import glfw                 # lean window system wrapper for OpenGL
+import numpy as np          # all matrix manipulations & OpenGL args
+from transform import translate, rotate, scale, vec, perspective
+from transform import Trackball, identity
 
 
-# ------------ low level OpenGL object wrappers ----------------------------
+# ------------ low level OpenGL object wrappers --------------------
 class Shader:
-    """ Helper class to create and automatically destroy shader program """
+    """ Helper class to create and automatically 
+            destroy shader program """
     @staticmethod
     def _compile_shader(src, shader_type):
         src = open(src, 'r').read() if os.path.exists(src) else src
@@ -54,32 +57,84 @@ class Shader:
         if self.glid:                      # if this is a valid shader object
             GL.glDeleteProgram(self.glid)  # object dies => destroy GL object
 
-
 # ------------  Simple color shaders ------------------------------------------
 COLOR_VERT = """#version 330 core
 layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 color;
+
+out vec3 fragColor;
+uniform mat4 matrix;
+
+
 void main() {
-    gl_Position = vec4(position, 1);
+    gl_Position =  matrix *vec4(position, 1);
+    fragColor = color;
 
 }"""
 
 COLOR_FRAG = """#version 330 core
-uniform vec3 color;
+in vec3 fragColor;
 out vec4 outColor;
 void main() {
-    outColor = vec4(1, 0, 0, 1);
-
-}"""
-COLOR_TEST = """#version 330 core
-uniform vec4 colors;
-out vec4 outColors;
-void main() {
-    outColors = vec4(1, 1, 0, 1);
+    outColor = vec4(fragColor, 1);
 
 }"""
 
 
-# ------------  Scene object classes ------------------------------------------
+# ------------  Scene object classes -----------------------------------------
+
+class VertexArray:
+    """ helper class to create and self destroy OpenGL vertex array objects. """
+    def __init__(self, attributes, index=None, usage=GL.GL_STATIC_DRAW):
+        """ Vertex array from attributes and optional index array. Vertex
+            Attributes should be list of arrays with one row per vertex. """
+
+        # create vertex array object, bind it
+        self.glid = GL.glGenVertexArrays(1)
+        GL.glBindVertexArray(self.glid)
+        self.buffers = []  # we will store buffers in a list
+        nb_primitives, size = 0, 0
+
+        # load buffer per vertex attribute (in list with index = shader layout)
+        for loc, data in enumerate(attributes):
+            if data is not None:
+                # bind a new vbo, upload its data to GPU, declare size and type
+                self.buffers += [GL.glGenBuffers(1)]
+                data = np.array(data, np.float32, copy=False)  # ensure format
+                nb_primitives, size = data.shape
+                GL.glEnableVertexAttribArray(loc)
+                GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.buffers[-1])
+                GL.glBufferData(GL.GL_ARRAY_BUFFER, data, usage)
+                GL.glVertexAttribPointer(loc, size, GL.GL_FLOAT, False, 0, None)
+
+        # optionally create and upload an index buffer for this object
+        self.draw_command = GL.glDrawArrays
+        self.arguments = (0, nb_primitives)
+        if index is not None:
+            self.buffers += [GL.glGenBuffers(1)]
+            index_buffer = np.array(index, np.int32, copy=False)  # good format
+            GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, self.buffers[-1])
+            GL.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, index_buffer, usage)
+            self.draw_command = GL.glDrawElements
+            self.arguments = (index_buffer.size, GL.GL_UNSIGNED_INT, None)
+
+        # cleanup and unbind so no accidental subsequent state update
+        GL.glBindVertexArray(0)
+        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+        GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, 0)
+
+    def execute(self, primitive):
+        """ draw a vertex array, either as direct array or indexed array """
+        GL.glBindVertexArray(self.glid)
+        self.draw_command(primitive, *self.arguments)
+        GL.glBindVertexArray(0)
+
+    def __del__(self):  # object dies => kill GL array and buffers from GPU
+        GL.glDeleteVertexArrays(1, [self.glid])
+        GL.glDeleteBuffers(len(self.buffers), self.buffers)
+
+
+
 class SimpleTriangle:
     """Hello triangle object"""
 
@@ -87,37 +142,94 @@ class SimpleTriangle:
 
         # triangle position buffer
         position = np.array(((0, .5, 0), (.5, -.5, 0), (-.5, -.5, 0)), 'f')
+        color = np.array(((1, 0, 0), (0, 1, 0), (0, 0, 1)), 'f')
 
-        self.glid = GL.glGenVertexArrays(1)  # create OpenGL vertex array id
-        GL.glBindVertexArray(self.glid)      # activate to receive state below
-        self.buffers = [GL.glGenBuffers(1)]  # create buffer for position attrib
+        self.my_vertex_array = VertexArray([position, color])
 
-        # bind the vbo, upload position data to GPU, declare its size and type
-        GL.glEnableVertexAttribArray(0)      # assign to layout = 0 attribute
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.buffers[0])
-        GL.glBufferData(GL.GL_ARRAY_BUFFER, position, GL.GL_STATIC_DRAW)
-        GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, False, 0, None)
-
-        # cleanup and unbind so no accidental subsequent state update
-        GL.glBindVertexArray(0)
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
 
     def draw(self, projection, view, model, color_shader):
-        my_color_location = GL.glGetUniformLocation(color_shader.glid, 'color')
-        GL.glUniform3fv(my_color_location, 1, (0,0,1))
         GL.glUseProgram(color_shader.glid)
 
+
+        matrix_location = GL.glGetUniformLocation(color_shader.glid, 'matrix')
+        GL.glUniformMatrix4fv(matrix_location, 1, True, projection
+                @ view @ model)
+                #rotate((0, 0, 1), 15) @ scale(2,1,1))
+
+                
+
         # draw triangle as GL_TRIANGLE vertex array, draw array call
-        GL.glBindVertexArray(self.glid)
-        GL.glDrawArrays(GL.GL_TRIANGLES, 0, 3)
-        GL.glBindVertexArray(0)
-
-    def __del__(self):
-        GL.glDeleteVertexArrays(1, [self.glid])
-        GL.glDeleteBuffers(1, self.buffers)
+        self.my_vertex_array.execute(GL.GL_TRIANGLES)
 
 
-# ------------  Viewer class & window management ------------------------------
+class PyramidMixColor:
+    """Hello triangle object"""
+
+    def __init__(self):
+
+        # triangle position buffer
+
+        position = np.array(((-0.5, 0, 0.5),(-0.5, 0, -0.5), (0, 1, 0)
+            , (.5, 0, 0.5), (.5, 0, -0.5)), 'f')
+        index = np.array((1, 0, 2, 0, 3, 2, 4, 1, 2, 3, 4, 2), np.uint32)
+        color = np.array(((1, 0, 0), (0, 1, 0), (0, 0, 1), (0, 1,
+            1), (1, 1, 0)), 'f')
+
+        self.my_vertex_array = VertexArray([position, color], index)
+
+
+    def draw(self, projection, view, model, color_shader):
+        GL.glUseProgram(color_shader.glid)
+
+
+        matrix_location = GL.glGetUniformLocation(color_shader.glid, 'matrix')
+        GL.glUniformMatrix4fv(matrix_location, 1, True, projection
+                @ view @ model)
+                #rotate((0, 0, 1), 15) @ scale(2,1,1))
+
+                
+
+        # draw triangle as GL_TRIANGLE vertex array, draw array call
+        self.my_vertex_array.execute(GL.GL_TRIANGLES)
+
+
+
+class PyramidFaceColor:
+    """Hello triangle object"""
+
+    def __init__(self):
+
+        # triangle position buffer
+        position = np.array(((-0.5, 0, 0.5), (-0.5, 0, 0.5),
+            (-0.5, 0, -0.5), (-0.5, 0, -0.5),
+            (0, 1, 0), (0, 1, 0), (0, 1, 0), (0, 1, 0),
+            (.5, 0, 0.5), (.5, 0, 0.5),
+            (.5, 0, -0.5), (.5, 0, -0.5)), 'f')
+        index = np.array((2, 0, 4, 1, 8, 5, 10, 3, 6, 9, 11, 7), np.uint32)
+        color = np.array(((1, 0, 0), (0, 1, 0), (1, 0, 0), (0, 0, 1), (1, 0, 0), (0, 1, 0), (0, 0, 1), (0, 1,
+            1), (0, 1, 0), (0, 1, 1), (0, 0, 1), (0, 1, 1)), 'f')
+
+        self.my_vertex_array = VertexArray([position, color], index)
+
+
+
+    def draw(self, projection, view, model, color_shader):
+        GL.glUseProgram(color_shader.glid)
+
+
+        matrix_location = GL.glGetUniformLocation(color_shader.glid, 'matrix')
+        GL.glUniformMatrix4fv(matrix_location, 1, True, projection
+                @ view @ model)
+                #rotate((0, 0, 1), 15) @ scale(2,1,1))
+
+        # draw triangle as GL_TRIANGLE vertex array, draw array call
+                
+        self.my_vertex_array.execute(GL.GL_TRIANGLES)
+
+
+
+# ------------  Viewer class & window management --- salut
+# la 
 class Viewer:
     """ GLFW viewer window, with classic initialization & graphics loop """
 
@@ -130,6 +242,8 @@ class Viewer:
         glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
         glfw.window_hint(glfw.RESIZABLE, False)
         self.win = glfw.create_window(width, height, 'Viewer', None, None)
+        self.trackball = GLFWTrackball(self.win)
+
 
         # make win's OpenGL context current; no OpenGL calls can happen before
         glfw.make_context_current(self.win)
@@ -144,9 +258,9 @@ class Viewer:
 
         # initialize GL by setting viewport and default render characteristics
         GL.glClearColor(0.1, 0.1, 0.1, 0.1)
-
+        GL.glEnable(GL.GL_CULL_FACE)
         # compile and initialize shader programs once globally
-        self.color_shader = Shader(COLOR_VERT, COLOR_TEST)
+        self.color_shader = Shader(COLOR_VERT, COLOR_FRAG)
 
         # initially empty list of object to draw
         self.drawables = []
@@ -156,10 +270,14 @@ class Viewer:
         while not glfw.window_should_close(self.win):
             # clear draw buffer
             GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+            winsize = glfw.get_window_size(self.win)
+            view = self.trackball.view_matrix()
+            projection = self.trackball.projection_matrix(winsize)
+
 
             # draw our scene objects
             for drawable in self.drawables:
-                drawable.draw(None, None, None, self.color_shader)
+                drawable.draw(projection, view, identity(), self.color_shader)
 
             # flush render commands, and swap draw buffers
             glfw.swap_buffers(self.win)
@@ -174,15 +292,39 @@ class Viewer:
     def on_key(self, _win, key, _scancode, action, _mods):
         """ 'Q' or 'Escape' quits """
         if action == glfw.PRESS or action == glfw.REPEAT:
+            if key == glfw.KEY_T:
+                self.color=(0.0,0.0,1.0)
             if key == glfw.KEY_ESCAPE or key == glfw.KEY_Q:
                 glfw.set_window_should_close(self.win, True)
+
+class GLFWTrackball(Trackball):
+    """ Use in Viewer for interactive viewpoint control """
+
+    def __init__(self, win):
+        """ Init needs a GLFW window handler 'win' to register callbacks """
+        super().__init__()
+        self.mouse = (0, 0)
+        glfw.set_cursor_pos_callback(win, self.on_mouse_move)
+        glfw.set_scroll_callback(win, self.on_scroll)
+
+    def on_mouse_move(self, win, xpos, ypos):
+        """ Rotate on left-click & drag, pan on right-click & drag """
+        old = self.mouse
+        self.mouse = (xpos, glfw.get_window_size(win)[1] - ypos)
+        if glfw.get_mouse_button(win, glfw.MOUSE_BUTTON_LEFT):
+            self.drag(old, self.mouse, glfw.get_window_size(win))
+        if glfw.get_mouse_button(win, glfw.MOUSE_BUTTON_RIGHT):
+            self.pan(old, self.mouse)
+
+    def on_scroll(self, win, _deltax, deltay):
+        """ Scroll controls the camera distance to trackball center """
+        self.zoom(deltay, glfw.get_window_size(win)[1])
 
 
 # -------------- main program and scene setup --------------------------------
 def main():
     """ create a window, add scene objects, then run rendering loop """
     viewer = Viewer()
-
     # place instances of our basic objects
     viewer.add(SimpleTriangle())
 
@@ -194,4 +336,6 @@ if __name__ == '__main__':
     glfw.init()                # initialize window system glfw
     main()                     # main function keeps variables locally scoped
     glfw.terminate()           # destroy all glfw windows and GL contexts
+
+
 
