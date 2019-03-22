7af6eba778fc508ec451898d081cdbb217e84723
Sylfid <remy.lavainne@laposte.net>
2019-03-22 12:11:51 +0100

    merge

--- Grapique3D/viewerCorrection.py
+++ Grapique3D/viewerCorrection.py
@@ -116,21 +116,26 @@ class VertexArray:
 COLOR_VERT = """#version 330 core
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 color;
+layout(location = 2) in vec3 normal;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 out vec3 fragColor;
+out vec4 positionFrag;
 
 void main() {
     gl_Position = projection * view * model * vec4(position, 1);
+    positionFrag = gl_Position;
     fragColor = color;
 }"""
 
 
 COLOR_FRAG = """#version 330 core
 in vec3 fragColor;
+in vec4 positionFrag;
 out vec4 outColor;
+
 void main() {
     outColor = vec4(fragColor, 1);
 }"""
