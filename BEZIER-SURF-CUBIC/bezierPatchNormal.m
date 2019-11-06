30902fd60f00fcbe4b9f507c8f6bb345089710dc
Sylfid <remy.lavainne@laposte.net>
2019-11-06 09:50:38 +0100

    avancement

--- BEZIER-SURF-CUBIC/bezierPatchNormal.m
+++ BEZIER-SURF-CUBIC/bezierPatchNormal.m
@@ -15,9 +15,24 @@ for i = 1:3
         Dy(j,i,3)=3*(B(j,i+1,3)-B(j,i,3));
     end
 end
+
+
+
+Vx = [Dx(i,j,1),Dx(i,j,2),Dx(i,j,3)];
+Vy = [Dy(i,j,1),Dy(i,j,2),Dy(i,j,3)];
     
 for i=1:lentgh(u)
     for j=1:length(v)
-        
+        for p=1:3
+            Bi=polyBernstein(p-1,2,1-1/i)
+            for q=1:4
+                for k=1:3
+                    N(i,j,k)=cross([])
+                end
+            end
+        end
     end
 end
+
+function x = polyBernstein(i,n,t)
+    x = factorial(n)/(factorial(i)*factorial(n-i))*((1-t).^(n-i))*(t.^i);
